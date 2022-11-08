ef93853fc62596fb8ca71d064a052eba77a18239
Zaman <m.zaman@ttu.edu>
2022-11-08 08:28:27 -0600

    Update

--- algorithm_1.py
+++ algorithm_1.py
@@ -15,3 +15,10 @@ for j in range(n):
     jobs.append(Job(j))
 
 # Algorithm start here
+def algorithm1(m, n):
+
+    # Initialization
+    trace_table = np.zeros((1,2*m+1))
+    print(trace_table[0])
+
+algorithm1(2,10)
